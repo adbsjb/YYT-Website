fef119a0657ea41cfc7d822a737280d84f9f3f8b
Alex Ball <alex@ballfamily.info>
2016-05-18 16:20:30 +0100

    removed margin on a h1

--- index.php
+++ index.php
@@ -93,19 +93,18 @@
 		</a>
 	</div>
 </div>
-<<<<<<< HEAD
+
 <div id="aboutUs">
 	<h1>Welcome to Yeovil youth theatre</h1>
 	<p>Now that we know who you are, I know who I am. I'm not a mistake! It all makes sense! In a comic, you know how you can tell who the arch-villain's going to be? He's the exact opposite of the hero. And most times they're friends, like you and me! I should've known way back when... You know why, David? Because of the kids. They called me Mr Glass.</p>
 </div>
 <div id="ourSponsers"><div>
-=======
+
 <div id="aboutUs"></div>
 <div id="ourSponsers">
 <?php
 	include 'sponserBanner.php'
 ?>
 	<div>
->>>>>>> origin/master
 </body>
 </html>
