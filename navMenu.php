f9bbeafe7667e2d2db6b9713936a7b3d912182b4
Extome <matt@bes.prestel.co.uk>
2016-05-25 21:27:36 +0100

    My webpage

--- navMenu.php
+++ navMenu.php
@@ -4,7 +4,7 @@ echo'
   <ul>
 <li><a href="whatsOn.html">What\'s On</a></li>
 <li><a href="aboutUs.html">About Us</a></li>
-<li><a href="behindTheScenes.html">Behind the scenes</a></li>
+<li><a href="currentProduction.php">Behind the scenes</a></li>
 <li><a href="pastProductions.html">Our Past Productions</a></li>
 <li><a href="alumni.html">Alumni</a></li>
 <li><a href="contactUs.html">Contact Us</a></li>
