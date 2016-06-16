10eb349242771c20fdda58dfb23789551f499cc1
Alex Ball <alex@ballfamily.info>
2016-06-16 01:23:15 +0100

    Added contact us page
    Its not great but it will do for now. Currently has a temporary email
    address in that button. Not sure which one Alan wants us to use

--- navMenu.php
+++ navMenu.php
@@ -7,7 +7,7 @@ echo'
 <li><a href="currentProduction.php">Behind the scenes</a></li>
 <li><a href="pastProductions.html">Our Past Productions</a></li>
 <li><a href="alumni.html">Alumni</a></li>
-<li><a href="contactUs.html">Contact Us</a></li>
+<li><a href="contactUs.php">Contact Us</a></li>
 <li><a href="words.html">Members login</a></li>
 </ul>
 
